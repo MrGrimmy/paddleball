713c144f10083ba9008476798ab0ebdb817fdc76
MrGrimmy <MrGrimmy@users.noreply.github.com>
2026-04-14 16:34:01 +0200

    Test Commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -50,7 +50,7 @@ public class GameManager : MonoBehaviour
             rightPaddleScore = ++rightPaddleScore;
         leftScore.text = leftPaddleScore.ToString();
         rightScore.text = rightPaddleScore.ToString();
-
+        string test = "test";
     }
     void Update()
     {
